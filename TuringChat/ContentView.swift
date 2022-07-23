5e95c545cd90426feaca5fefc3334f0a2d78f2e0
adoniscx <adoniscx@gmail.com>
2022-07-23 12:09:15 +0800

    first commit

---
+++ TuringChat/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  TuringChat
+//
+//  Created by xu cao on 2022/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State var message = ""
+    @StateObject var messages = Messages()
+
+    var body: some View {
+        VStack{
+            ScrollView(.vertical, showsIndicators: false){
+                ScrollViewReader { proxy in
+                    VStack{
+                        ForEach(messages.messages) {msg in
+                            ChatBubble(message: msg)
+                        }
+                    }
+                }
+            }
+            HStack(){
+                TextField("your turn", text: $message)
+                Button("Send") {
+                    messages.writeMessage(msg: message)
+                }
+            }
+            .background(Color.black.opacity(0.06))
+        }
+    }
+}
+
+struct Message : Identifiable {
+    var id : Date
+    var message : String
+}
+
+struct ChatBubble : View {
+    var message : Message
+    var body: some View {
+        Text(message.message)
+    }
+}
+class Messages : ObservableObject {
+    @Published var messages : [Message] = []
+
+    func writeMessage(msg: String) {
+        messages.append(Message(id: Date(), message: msg))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ TuringChat/TuringChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuringChatApp.swift
+//  TuringChat
+//
+//  Created by xu cao on 2022/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TuringChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
